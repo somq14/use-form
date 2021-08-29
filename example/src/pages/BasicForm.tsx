107c11a271f9cd1d47f765c70c4c938475cc97d2
somq14 <som.q14@gmail.com>
2021-08-29 15:04:20 +0900

    tmp

--- example/src/pages/BasicForm.tsx
+++ example/src/pages/BasicForm.tsx
@@ -1,35 +1,36 @@
-// import { useForm, bindField } from "@somq14/use-form";
-import { NumberType } from "@somq14/use-form";
+import { useForm, bindField } from "@somq14/use-form";
 import React from "react";
 import "./BasicForm.css";
 
-// type LoginForm = {
-//   userId: string;
-//   password: string;
-// };
+type LoginForm = {
+  userId: string;
+  password: string;
+};
 
 export const BasicForm: React.FC = () => {
-  // const { form, validated } = useForm<LoginForm>({
-  //   userId: {},
-  //   password: {},
-  // });
-  // const onSubmit = (e: React.FormEvent<HTMLFormElement>) => {
-  //   e.preventDefault();
-  //   const formValue = validated();
-  //   alert(`userId: ${formValue.userId} password: ${formValue.password}`);
-  // };
-  // return (
-  //   <div className="basic-form">
-  //     <h1>basic form</h1>
-  //     <form onSubmit={onSubmit}>
-  //       <label htmlFor="user-id">USER ID</label>
-  //       <input id="user-id" type="text" {...bindField(form.userId)} />
-  //       <label htmlFor="password">PASSWORD</label>
-  //       <input id="password" type="password" {...bindField(form.userId)} />
-  //       <input type="submit"></input>
-  //     </form>
-  //   </div>
-  // );
-  console.info(NumberType);
-  return <div></div>;
+  const { form, validated } = useForm<LoginForm>({
+    userId: {},
+    password: {},
+  });
+
+  const onSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    const formValue = validated();
+    alert(`userId: ${formValue.userId} password: ${formValue.password}`);
+  };
+
+  return (
+    <div className="basic-form">
+      <h1>basic form</h1>
+      <form onSubmit={onSubmit}>
+        <label htmlFor="user-id">USER ID</label>
+        <input id="user-id" type="text" {...bindField(form.userId)} />
+
+        <label htmlFor="password">PASSWORD</label>
+        <input id="password" type="password" {...bindField(form.password)} />
+
+        <input type="submit" />
+      </form>
+    </div>
+  );
 };
